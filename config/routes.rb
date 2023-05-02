cfcb655ef3a6fac95215d633a68097b28fad53c5
wtpn <wataru.noguchi@medley.jp>
2023-05-02 12:04:18 +0900

    演習:　Fooコントローラの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-  get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
