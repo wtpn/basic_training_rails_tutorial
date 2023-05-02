bed0f7d23473ca427bcbc1bcfcbe6633dec2feb2
wtpn <wataru.noguchi@medley.jp>
2023-05-02 12:03:15 +0900

    演習:　Fooコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+  get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
