60e14fa6ceb6668f28ab22b79f054aa1968798cb
wtpn <wataru.noguchi@medley.jp>
2023-05-02 16:11:51 +0900

    演習:　root_urlを使う

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
@@ -7,5 +8,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'application#hello'
 end
