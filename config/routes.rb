874865a0d6e36152517dc83f0658c9f9a522160f
wtpn <wataru.noguchi@medley.jp>
2023-05-09 10:48:14 +0900

    ユーザープロフィールページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
