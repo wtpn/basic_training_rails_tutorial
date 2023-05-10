4e4a338385556da698cde56493abff69236b1e10
wtpn <wataru.noguchi@medley.jp>
2023-05-10 10:43:51 +0900

    ユーザーの検索と認証

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def crreate
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+    render 'new'
+    end
+  end
+
+  def destroy
+
+  end
 end
