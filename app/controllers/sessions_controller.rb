520769f97bcf95d14fa47efcfe76961b48ee4470
wtpn <wataru.noguchi@medley.jp>
2023-05-10 10:57:38 +0900

    フラッシュメッセージとそのテストの追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,11 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def crreate
+  def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
     else
+      flash.now[:danger] = 'Invalid email/password combination'
     render 'new'
     end
   end
