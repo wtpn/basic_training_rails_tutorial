6f7e217981bbc21d58da50f1d5fd602da77537c5
wtpn <wataru.noguchi@medley.jp>
2023-05-11 11:15:52 +0900

    演習:　class << selfを使ってdigestとnew_tokenメソッドを定義する

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,5 +37,17 @@ class User < ApplicationRecord
   #   SecureRandom.urlsafe_base64
   # end
 
+  # class << self
+  #   def digest(string)
+  #     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+  #                                                   BCrypt::Engine.cost
+  #     BCrypt::Password.create(string, cost: cost)
+  #   end
+
+  #   def new_token
+  #     SecureRandom.urlsafe_base64
+  #   end
+  # end
+
   # ---------------------------------
 end
