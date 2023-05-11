d9f9a65b42e4386ef3484dd1db1f3e41355243fa
wtpn <wataru.noguchi@medley.jp>
2023-05-11 15:34:48 +0900

    remember meチェックボックスの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+    BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
