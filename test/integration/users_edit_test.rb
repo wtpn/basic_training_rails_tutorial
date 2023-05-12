000ae05bc754d6b44e14134adca632a24c33c576
wtpn <wataru.noguchi@medley.jp>
2023-05-12 15:09:56 +0900

    演習:　session[:forwarding_uel]が正しい値かどうか確認するテストの追加

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -26,6 +26,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     get edit_user_path(@user)
     log_in_as(@user)
     assert_redirected_to edit_user_url(@user)
+    assert_nil session[:forwarding_url]
     name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: {
