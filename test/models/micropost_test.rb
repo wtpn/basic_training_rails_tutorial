ceb30a372ee5b44bfbddbbf4ee09e318e11f4df3
wtpn <wataru.noguchi@medley.jp>
2023-05-15 15:47:17 +0900

    default scopeの追加、dependent: destroyの追加、テストの追加

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,6 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:michael)
-    # このコードは慣習的に正しくない
     @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
@@ -25,5 +24,9 @@ class MicropostTest < ActiveSupport::TestCase
   test "content should be at most 140 characters" do
     @micropost.content = "a" * 141
     assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
   end
 end
