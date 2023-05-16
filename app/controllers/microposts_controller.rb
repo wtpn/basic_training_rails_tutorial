0684d77290dfd614b2ce81d0d17dca3b00410535
wtpn <wataru.noguchi@medley.jp>
2023-05-16 11:08:17 +0900

    演習:　有効な送信を受けた後の処理をコメントアウトしたらエラー

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,8 +5,8 @@ class MicropostsController < ApplicationController
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] = "Micropost created!"
-      redirect_to root_url
+      # flash[:success] = "Micropost created!"
+      # redirect_to root_url
     else
       @feed_items = []
       render 'static_pages/home'
