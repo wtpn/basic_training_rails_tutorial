a54696aa862bc7e805c823aecaa71db05314956e
wtpn <wataru.noguchi@medley.jp>
2023-05-16 15:46:41 +0900

    Userモデルにfollowingの関連付けを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   attr_accessor :remember_token
 
