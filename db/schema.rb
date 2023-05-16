5569affd184aecbda41ac08f6b82e40cee7b70b8
wtpn <wataru.noguchi@medley.jp>
2023-05-16 11:25:28 +0900

    Micropostモデルにpictureカラム（string）を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_15_015619) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_022333) do
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
