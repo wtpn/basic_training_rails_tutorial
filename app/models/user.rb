c923cc4a45e2ea4307d1181ae8e2902fe4e78553
wtpn <wataru.noguchi@medley.jp>
2023-05-17 16:58:56 +0900

    有効化のテストとリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,6 +47,17 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  # アカウントを有効にする
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # 有効化用のメールを送信する
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   # 試作feed
   def feed
     following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
