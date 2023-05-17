69db70509b9cbf06eb826868f380219f65f33068
wtpn <wataru.noguchi@medley.jp>
2023-05-17 14:45:16 +0900

    データベースのリセット、テストデータの変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,9 @@ User.create!(
   email: "example@railstutorial.org",
   password: "foobar",
   password_confirmation: "foobar",
-  admin: true
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now
 )
 
 99.times do |n|
@@ -15,7 +17,9 @@ User.create!(
   name: name,
   email: email,
   password: password,
-  password_confirmation: password
+  password_confirmation: password,
+  activated: true,
+  activated_at: Time.zone.now
 )
 end
 
