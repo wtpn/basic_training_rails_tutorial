2d6067e1050bbf021c9f7bb1d32f5c06d565c146
wtpn <wataru.noguchi@medley.jp>
2023-05-17 15:27:54 +0900

    演習:　CGI.escapeをしないとテストは通らない

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -12,5 +12,6 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match user.name,               mail.body.encoded
     assert_match user.activation_token,   mail.body.encoded
     assert_match CGI.escape(user.email),  mail.body.encoded
+    # assert_match user.email, mail.body.encoded
   end
 end
